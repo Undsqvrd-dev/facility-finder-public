036abf354733c28cb74b751ad57145cb90aad14a
Steijn de Ridder <steijn@undsqvrd.nl>
2025-06-25 20:30:57 +0200

    Verwijder AuthProvider uit _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
-import { AuthProvider } from '../contexts/AuthContext';
 import { pageview } from '../lib/gtm';
 import { Analytics } from '@vercel/analytics/next';
 import '../styles/globals.css';
@@ -23,10 +22,10 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [router.events]);
 
   return (
-    <AuthProvider>
+    <>
       <Component {...pageProps} />
       <Toaster />
       <Analytics />
-    </AuthProvider>
+    </>
   );
 } 
