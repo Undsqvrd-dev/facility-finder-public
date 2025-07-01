1958dd42c32ebd2dd7412e6b9c9082e7dea1c193
Steijn de Ridder <steijn@undsqvrd.nl>
2025-07-01 12:41:25 +0200

    Verwijder dubbele Google Analytics code uit _document.tsx, alleen tracking via _app.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,18 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Google Analytics 4 */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-481R98VMEP"></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-481R98VMEP', { anonymize_ip: true });
-              `,
-            }}
-          />
+          {/* Hier GEEN Google Analytics meer, alles via _app.tsx */}
         </Head>
         <body>
           {/* Google Tag Manager (noscript) */}
