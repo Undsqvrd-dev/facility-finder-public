8cebaa95bdcb4c9a30d8c1d1bebdf06a8cc14cbf
Steijn de Ridder <steijn@undsqvrd.nl>
2025-07-01 12:26:44 +0200

    Google Analytics 4 gtag.js direct in <Head> van _document.tsx voor gegarandeerde detectie

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,15 +5,15 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Google Tag Manager */}
+          {/* Google Analytics 4 */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-481R98VMEP"></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','G-481R98VMEP');
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-481R98VMEP', { anonymize_ip: true });
               `,
             }}
           />
