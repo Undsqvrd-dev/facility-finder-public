5411ed07135bb58d9bc7a3197a49997dd9fca91f
Steijn de Ridder <steijn@undsqvrd.nl>
2025-07-03 14:03:42 +0200

    Google Analytics en Vercel Analytics toegevoegd aan /pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css';
 import { useEffect } from 'react';
+import Script from 'next/script';
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
+  if (typeof window !== 'undefined') {
+    console.log('✅ /pages/_app.js wordt uitgevoerd!');
+  }
+
   useEffect(() => {
     // Force a repaint to ensure styles are applied
     document.body.style.display = 'none';
@@ -9,7 +15,25 @@ function MyApp({ Component, pageProps }) {
     document.body.style.display = '';
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics 4 */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-481R98VMEP`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-481R98VMEP', { anonymize_ip: true });
+        `}
+      </Script>
+      <Component {...pageProps} />
+      <VercelAnalytics />
+    </>
+  );
 }
 
 export default MyApp;
