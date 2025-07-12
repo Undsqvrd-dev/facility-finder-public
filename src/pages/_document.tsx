fe1b9f55f01ad64a0b3530a596793c840fd94ead
Steijn de Ridder <steijn@undsqvrd.nl>
2025-07-12 08:35:48 +0200

    Favicon en Open Graph meta tags toegevoegd aan _document.tsx voor optimale deelbaarheid

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,30 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Hier GEEN Google Analytics meer, alles via _app.tsx */}
+          {/* Favicon */}
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/logos/Favicon.png" />
+          <link rel="icon" type="image/svg+xml" href="/logos/Favicon.svg" />
+
+          {/* Open Graph Meta Tags voor betere link sharing */}
+          <meta property="og:title" content="Facility Finder - Vind facilitaire organisaties" />
+          <meta property="og:description" content="Ontdek facilitaire organisaties via onze interactieve kaart. Handig voor bijbanen, stages en banen in de facilitaire branche." />
+          <meta property="og:image" content="https://www.facilityfinder.nl/logos/preview.png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://www.facilityfinder.nl" />
+
+          {/* Twitter Card Meta Tags */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Facility Finder - Vind facilitaire organisaties" />
+          <meta name="twitter:description" content="Ontdek facilitaire organisaties via onze interactieve kaart. Handig voor bijbanen, stages en banen in de facilitaire branche." />
+          <meta name="twitter:image" content="https://www.facilityfinder.nl/logos/preview.png" />
+
+          {/* Additional Meta Tags */}
+          <meta name="description" content="Ontdek facilitaire organisaties via onze interactieve kaart. Handig voor bijbanen, stages en banen in de facilitaire branche." />
+          <meta name="keywords" content="facility management, facilitaire organisaties, stages, bijbanen, banen, FM" />
+          <meta name="author" content="UNDSQVRD" />
         </Head>
         <body>
           {/* Google Tag Manager (noscript) */}
